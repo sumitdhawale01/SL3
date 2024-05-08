ea6bb0fdbc9ce6c20a0690ddbf764c22907c18c6
sumitdhawale01 <sumitdhawale01@gmail.com>
2024-05-09 00:42:54 +0530

    changes updated

---
+++ DS_12(2)_if-else(take any one).scala
@@ -0,0 +1,19 @@
+
+object Test { 
+	
+// Main Method 
+def main(args: Array[String]) { 
+	var a: Int = 650
+
+	if (a > 698) 
+	{ 
+		println("Pass") 
+	} 
+	
+	else
+	{ 
+		println("You are Fail") 
+	} 
+} 
+} 
+
---
+++ DS_12_addition(take any one).scala
@@ -0,0 +1,10 @@
+object Main  
+{ 
+    def main(args: Array[String])  
+    { 
+        var p = 10
+        var q = 30
+        var sum = p + q 
+        println("The sum of p and q is :"+sum); 
+    } 
+} 
